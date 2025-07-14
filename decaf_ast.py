8b6b360b122e004ea07b7b2ed539ff68c8270efa
Aawab Mahmood <76081184+aawab@users.noreply.github.com>
2025-07-14 19:05:50 -0400

    Update decaf_ast.py

--- decaf_ast.py
+++ decaf_ast.py
@@ -1,5 +1,5 @@
-# Vincent Zheng, vinzheng, 113469839
-# Aawab Mahmood, aamahmood, 113472709
+# Vincent Zheng
+# Aawab Mahmood
 
 # Global vars to keep track of curr method, class, etc. for typechecking
 
